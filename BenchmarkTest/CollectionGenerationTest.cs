68fc1a4dc62192d332f5c50fa3d160fff1606404
William Prussak Macarini <william.macarini@ambevtech.com.br>
2024-05-20 00:05:50 -0300

    Add project files.

---
+++ BenchmarkTest/CollectionGenerationTest.cs
@@ -0,0 +1,77 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Collections.ObjectModel;
+
+namespace BenchmarkTest
+{
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class CollectionGenerationTest
+    {
+        [Benchmark]
+        public void NumbersList()
+        {
+            var numbersList = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                numbersList.Add(i);
+            }
+        }
+
+        [Benchmark]
+        public void NumbersCollection()
+        {
+            var numbersCollection = new Collection<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                numbersCollection.Add(i);
+            }
+        }
+
+        [Benchmark]
+        public void NumbersArray()
+        {
+            var numbersArray = new int[100];
+            for (int i = 0; i < 99; i++)
+            {
+                numbersArray[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public void NumbersSpan()
+        {
+            var numbersSpan = new Span<int>([99]);
+            for (int i = 0; i < numbersSpan.Length; i++)
+            {
+                numbersSpan[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public void NumbersStackAlloc()
+        {
+            Span<int> number = stackalloc int[99];
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public void NumbersStruct()
+        {
+            var number = new IntTest[100];
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = new IntTest(i);
+            }
+        }
+
+        internal struct IntTest(int number)
+        {
+            public int Number { get; set; } = number;
+        }
+    }
+}
---
+++ BenchmarkTest/StringTest.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkTest
+{
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    public class StringTest
+    {
+        public string FirstProperty { get; } = "Test";
+        public string SecondProperty { get; } = "Test";
+
+        [Benchmark]
+        public bool StringEquals()
+            => FirstProperty.ToLower() == SecondProperty.ToLower();
+
+        [Benchmark]
+        public bool StringEqualsComparison()
+            => FirstProperty.Equals(SecondProperty, StringComparison.OrdinalIgnoreCase);
+    }
+}
